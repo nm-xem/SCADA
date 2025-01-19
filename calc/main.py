3dc909b9289ce5c0586df4c966ff564e5c5c1ae0
Nikita Antipin <nm_antipin@mail.ru>
2025-01-19 14:54:41 +0300

    Добавен цикл запуска модулей с обработкой событий.

--- calc/main.py
+++ calc/main.py
@@ -7,8 +7,24 @@ sys.path.append(f"{main_dir}")
 
 from secondary_functions import protocol_functions as pf
 name_module = 'main_module'
+error = False
 
 if __name__ == '__main__':
-    Calc_mm = Calculation_modules_manager()
+    list_modules = [Calculation_modules_manager()]
+    for module in list_modules:
+        try:
+            module.run()
+        except:
+            pf.write_file_log(name_module,f'Не удалось запустить модуль "{module.name_module}"')
+            for module in list_modules:
+                try:
+                    module.threads_stop = True
+                except: pass
+            error = True
+            break
+    else:
+        pf.write_file_log(name_module,f'Все модули успешно запущены')
     while True:
+        if error:
+            break
         time.sleep(10)
